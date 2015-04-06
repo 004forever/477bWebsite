e968dc1bcac071383dacdbbdf96ca7887518ac9e
Roy Zheng <roycraft3@gmail.com>
2015-04-06 00:58:35 -0700

    style

--- analyzer/src/router.php
+++ analyzer/src/router.php
@@ -1,20 +1,22 @@
-<?php
+Ï<?php
 
-class Router{
-public static function shortestPath(&$car, &$start){
-return array_reverse(Router::dfs($car->destination,$start));
-}
-public static function dfs(&$dest,&$cur){
-$cur->discovered=true;
-foreach($cur->connections as &$conn){
-$a = dfs($dest,$conn->end);
-if($a){
-$a[] = $cur;
-return $a;
-}
-}
-return null;
-}
-}
+class Router {
 
+    public static function shortestPath(&$car, &$start) {
+        return array_reverse(Router::dfs($car->destination, $start));
+    }
+
+    public static function dfs(&$dest, &$cur) {
+        $cur->discovered = true;
+        foreach ($cur->connections as &$conn) {
+            $a = dfs($dest, $conn->end);
+            if ($a) {
+                $a[] = $cur;
+                return $a;
+            }
+        }
+        return null;
+    }
+
+}
 ?>
